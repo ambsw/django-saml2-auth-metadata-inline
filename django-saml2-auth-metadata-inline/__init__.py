5ea593e5f65345ae985c118720153302f428ffeb
Clayton Daley <clayton.daley@gmail.com>
2019-09-20 14:12:12 -0500

    update to classmethod strategy

--- django-saml2-auth-metadata-inline/__init__.py
+++ django-saml2-auth-metadata-inline/__init__.py
@@ -8,7 +8,8 @@ from django_saml2_auth.plugins.metadata import MetadataPlugin
 class DefaultMetadataHandler(MetadataPlugin):
     NAME = 'INLINE'
 
-    def get_metadata(self):
+    @classmethod
+    def get_metadata(cls):
         if 'METADATA_XML_STRING' in settings.SAML2_AUTH:
             return {
                 'inline': [settings.SAML2_AUTH['METADATA_XML_STRING']]
